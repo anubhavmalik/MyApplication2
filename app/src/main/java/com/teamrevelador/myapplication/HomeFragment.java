916ee916f5e1cad3a427fbfaed64e6bd50c492ed
Anubhav <anubhavmalikdeveloper@gmail.com>
2018-03-31 16:33:02 +0530

    Initial Commit
    Sample usage of navigation drawer,bottom nav, bottomsheet & tabbed view

---
+++ app/src/main/java/com/teamrevelador/myapplication/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.teamrevelador.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anubhavmalik on 31/03/18.
+ */
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_frag_layout,container,false);
+
+        return view;
+    }
+}
